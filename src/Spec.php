e0147478ee1acdab17d3e5f09a5a4079a2b153fd
Anthony Baggett <anthony.baggett@gmail.com>
2023-12-16 12:12:51 -0600

    Initial commit

---
+++ src/Spec.php
@@ -0,0 +1,69 @@
+<?php
+namespace Code4recovery\App;
+
+class C4RSoec
+{
+    private static $languages = [
+        'en' => 'English',
+        'es' => 'Español',
+        'fr' => 'Français',
+        'js' => '日本語',
+        'sv' => 'Svenska',
+    ];
+
+    private static $types;
+
+    /**
+     * Constructor.
+     *
+     * @param $program
+     */
+    public function __construct() {
+        if($jsonTypes = file_get_contents('./src/types.json')) {
+            self::$types = json_decode($jsonTypes);
+        }
+    }
+
+    /**
+     * Get all languages.
+     *
+     * Retrieves and returns all languages with language code and expanded language translation that is stored
+     * in the type repository.
+     *
+     * @return string[]
+     */
+    public function getLanguages()
+    {
+        return self::$languages;
+    }
+
+    /**
+     * Get all types.
+     *
+     * Retrieves and returns all types stored in the type repository.
+     *
+     * @return array The array of all types.
+     */
+    public function getAllTypes ()
+    {
+        return self::$types;
+    }
+
+    /**
+     * Returns an array of types translated into the specified language.
+     *
+     * @param string $language The language code to retrieve the translations
+     *     for.
+     *
+     * @return array An array of type translations in the specified language.
+     */
+    public function getTypesByLanguage($language)
+    {
+        foreach (self::$types as $typeKey => $typeTranslations) {
+            if (isset($typeTranslations->$language)) {
+                $typesByLanguage[$typeKey] = $typeTranslations->$language;
+            }
+        }
+        return $typesByLanguage;
+    }
+}
